6eeb63ed28fc1f5fd797778568c00bfe0a7c14f0
Leo.N <leo.nordstrand1@skola.taby.se>
2024-02-16 19:03:29 +0100

    experiment stuff

--- main.py
+++ main.py
@@ -39,13 +39,14 @@ from simulation.event_handler import handle_events, delegate_event
 # TODO: TYPE ANNOTATIONS!
 # TODO: Skapa ett sätt för användaren att mäta tid
 # TODO: Skapa ett sätt för användaren att mäta distans
-# TODO: Skapa ett sätt för användaren att inspektera objekt och se deras egenskaper
+
 # TODO: Skapa ett sätt för användaren att kontrollera objekt
+# TODO: Skapa godtycklig polygon
 
 
-os.environ[
-    "SDL_VIDEO_WINDOW_POS"
-] = "1920, 0"  # Kommentera bort ifall man inte vill att fönstret ska öppnas på en annan skärm
+# os.environ[
+#     "SDL_VIDEO_WINDOW_POS"
+# ] = "1920, 0"  # Kommentera bort ifall man inte vill att fönstret ska öppnas på en annan skärm
 
 
 def main():
@@ -85,13 +86,35 @@ def main():
         position=Vector2D(200, 400),
         force=Vector2D(50, 50),
     )
+    
+    # Experiment Objekt
+    balanceBoardBase = ConvexPolygon(
+        mass=1,
+        color=YELLOW,
+        position=Vector2D(200 + ORIGIN[0], 100 + ORIGIN[1]),
+        num_of_sides=3,
+        side_length=50,
+        angle=math.pi / 2,
+        is_static=True,
+    )
+    balanceBoard = ConvexPolygon(
+        mass=1,
+        color=RED,
+        width=200,
+        height=10,
+        position=Vector2D(200 + ORIGIN[0], 128 + 5 + ORIGIN[1]),
+    )
 
-    # state.add_objects([rect1])
+    state.add_objects([balanceBoard, balanceBoardBase])
+
+    experiment_start_time = 0 # NOTE: gjord för experiment
+    experiment_end_time = 0 # NOTE: gjord för experiment
+    current_experiment_start_time = 0 # NOTE: gjord för experiment
 
     running = True
     prev_time = time.time()
     frame_limit = 1 / 60
-    state.generate_test_data(scale=5)
+    # state.generate_test_data(scale=5
     while running:
         ui_refresh_rate = clock.tick(FPS) / 1000
         # TODO: Omstrukturera tid in i egen funktion/klass
@@ -107,10 +130,18 @@ def main():
 
         delegate_event(event, state, view_model, ui_manager)
         
+        # experiment_code(state) # NOTE: gjord för experiment
+        if state.is_paused: # NOTE: gjord för experiment
+            current_experiment_start_time = time.time() # NOTE: gjord för experiment
+        
         # Slutar beräkna fysiken ifall användaren har pausat simulationen
         if not state.is_paused: 
             state.step(delta_time=dt, iterations=RESOLUTION_ITERATIONS)
             state.handle_collisions()
+            experiment_end_time = time.time() # NOTE: gjord för experiment
+            experiment_start_time = current_experiment_start_time
+        
+        stop_watch(view_model, experiment_start_time, experiment_end_time) # NOTE: gjord för experiment
 
         view_model.render_or_uptade_selected_object_related()
 
@@ -125,5 +156,22 @@ def main():
     pygame.quit()
 
 
+# NOTE: Allt under förutom main loopen är slarvig kod för att utföra experiment
+def experiment_code(state):
+    if len(state.objects) > 0:
+        if state.objects[-1].position.y < 100 + ORIGIN[1]:
+            state.is_paused = True
+
+def stop_watch(view_model, experiment_start_time, experiment_end_time):
+        view_model.render_text(
+                    f"Stopwatch: {experiment_end_time - experiment_start_time}",
+                    BLACK,
+                    (10, 1000),
+                    20,
+                )
+
 if __name__ == "__main__":
     main()
+
+
+    
